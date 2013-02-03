96c7125af5166d9a10f27bfdef5f6351645fd410
Tim Miller <tim.miller.0@gmail.com>
2013-02-02 20:12:28 -0800

    hacky update hook to listen to github

--- app.rb
+++ app.rb
@@ -2,6 +2,7 @@
 $:.push "#{File.dirname __FILE__}/lib"
 require 'sinatra'
 require 'glorify'
+require 'json'
 
 Tilt.prefer Sinatra::Glorify::Template
 
@@ -20,6 +21,30 @@ end
 get '/post/:post' do
   #markdown params[:post].to_sym, :layout => :layout
   markdown "posts/#{params[:post]}".to_sym, :layout => :layout
+end
+
+post '/__push__' do
+  begin
+    puts "foo"
+    puts params[:payload]
+    puts "foo"
+    push = JSON.parse(params[:payload])
+    puts "foo"
+    puts push.inspect
+    puts "foo"
+    puts "foo"*30
+    puts push['repository']['name']
+    puts push['ref']
+    if push['repository']['name'] == 'echohead.org' and push['ref'] == 'refs/heads/master'
+      puts "updating."
+      fork do
+        exec "#{File.expand_path(File.dirname __FILE__)}/bin/update"
+      end
+    end
+    "ok"
+  rescue
+    halt 400
+  end
 end
 
 configure do
